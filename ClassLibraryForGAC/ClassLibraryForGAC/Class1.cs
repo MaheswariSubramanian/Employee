19773f38351333c5ccc6e243225c65fe90d73a61
MaheswariSubramanian <53537537+MaheswariSubramanian@users.noreply.github.com>
2020-01-16 10:43:15 +0530

    Add files via upload

---
+++ ClassLibraryForGAC/ClassLibraryForGAC/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ClassLibraryForGAC
+{
+    public class TestClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Hi You Accessed me");
+        }
+    }
+}
